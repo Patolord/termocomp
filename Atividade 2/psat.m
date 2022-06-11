beb3b9309cf9f9e0247fcdf3ee6b7008de917bdb
rodri <rodri@DESKTOP-37GDGKG>
2022-06-10 21:42:20 -0300

    Ativ2 PR e SRK

--- Atividade 2/psat.m
+++ Atividade 2/psat.m
@@ -3,33 +3,6 @@ load Dados/tcpcwch4.txt
 load Dados/parantoinech4.txt
 load Dados/psatch4.txt
 
-% pexp=psatch4(:,2).*1000;
-% texp=psatch4(:,1);
-% nexp=length(texp);
-% tc=tcpcwch4(1,1);
-% pc=tcpcwch4(1,2);
-% w=tcpcwch4(1,3);
-% pa=parantoinech4;
-% 
-% tmin=psatch4(1,1);
-% tmax=psatch4(nexp,1);
-%  
-% load tcpcwacetona.txt
-% load parantoineacetona.txt
-% load psatacetona.txt
-% 
-% pexp=psatacetona(:,2).*1000;
-% texp=psatacetona(:,1);
-% nexp=length(texp);
-% 
-% tc=tcpcwacetona(1,1);
-% pc=tcpcwacetona(1,2);
-% w=tcpcwacetona(1,3);
-% pa=parantoineacetona;
-% 
-% tmin=psatacetona(1,1);
-% tmax=psatacetona(nexp,1);
-
 load Dados/tcpcwh2o.txt
 load Dados/parantoineh2o.txt
 load Dados/psath2o.txt
@@ -51,7 +24,6 @@ tmax=psath2o(nexp,1);
 if tmax<tc
 nt=100;
 [tcalc]=linspace(tmin,tmax,nt);
-[apr,bpr]=calcabpr(tcalc,tc,pc,w);
 psata=zeros(nt,1);
 psatpr=zeros(nt,1);
 psatsrk=zeros(nt,1);
@@ -66,7 +38,7 @@ chutepr=psata(1);
 
 for k=1:nt
 disp(tcalc(k))
-[psatpr(k)]=calculaelvfipr(tcalc(k),apr(k),bpr,chutepr);
+[psatpr(k)]=calcpsatpr(tcalc(k),tc,pc,w,chutepr);
 [psatsrk(k)]=calcpsatsrk(tcalc(k),tc,pc,w,chutepr);
 
 chutepr=psatpr(k);
